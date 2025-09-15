542f7918bf3b6a450a91242340d86ae2553a7fc8
Criticyl <criticyl@gmail.com>
2025-09-15 15:40:09 +0100

    Added Window header.

---
+++ Sakura/src/Window.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace Sakura {
+
+    struct WindowProperties
+    {
+        int Width = 1280;
+        int Height = 720;
+        std::string Title = "Sakura | Voxel Engine";
+    };
+
+    class Window
+    {
+    public:
+        Window(const WindowProperties& properties)
+            : m_Width(properties.Width),
+              m_Height(properties.Height),
+              m_Title(properties.Title)
+        { 
+            Init();
+        }
+
+        ~Window();
+    private:
+
+        void Init();
+
+        int m_Width;
+        int m_Height;
+        std::string m_Title;
+        GLFWwindow* m_Window;
+    };
+
+}
